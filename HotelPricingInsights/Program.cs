6985a1f93213eaaf9bc8fd3643557691268625aa
Meester Stefan <stefan.billiet@corilus.be>
2025-11-04 11:44:56 +0100

    Add OpenTelemetry for request duration insights

--- HotelPricingInsights/Program.cs
+++ HotelPricingInsights/Program.cs
@@ -1,10 +1,15 @@
 using HotelPricingInsights;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddPricingComparisonServices();
+ConfigureOpenTelemetry(builder);
 
 var app = builder.Build();
 
@@ -19,7 +24,53 @@ app.MapControllers();
 
 app.Run();
 
+//OTEL will give you all the insights needed about how long requests etc. take, as long as you have a backend to send the data to.
+static void ConfigureOpenTelemetry(WebApplicationBuilder builder)
+{
+    var assembly = typeof(HotelPricingInsights.Program).Assembly;
+    var serviceName = assembly.GetName().Name ?? "HotelPricingInsights";
+    var serviceVersion = assembly.GetName().Version?.ToString() ?? "1.0.0";
+
+    var resourceBuilder = ResourceBuilder.CreateDefault()
+        .AddService(serviceName: serviceName, serviceVersion: serviceVersion, serviceInstanceId: Environment.MachineName);
+
+    builder.Services.AddOpenTelemetry()
+        .WithTracing(tracing =>
+        {
+            tracing
+                .SetResourceBuilder(resourceBuilder)
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri("http://localhost:4317");
+                });
+        })
+        .WithMetrics(metrics =>
+        {
+            metrics
+                .SetResourceBuilder(resourceBuilder)
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddRuntimeInstrumentation()
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri("http://localhost:4317");
+                });
+        });
+
+    builder.Logging.AddOpenTelemetry(logging =>
+    {
+        logging.SetResourceBuilder(resourceBuilder);
+        logging.IncludeScopes = true;
+        logging.AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://localhost:4317");
+        });
+    });
+}
+
 namespace HotelPricingInsights
 {
-    public partial class Program;
+    public class Program;
 }

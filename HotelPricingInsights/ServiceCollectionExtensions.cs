6929596cb44ee60234777883ab0f4b3b0fc7e4f5
Meester Stefan <stefan.billiet@corilus.be>
2025-11-04 12:16:51 +0100

    Forgot to register the caching CurrencyExchangeRatesDataService

--- HotelPricingInsights/ServiceCollectionExtensions.cs
+++ HotelPricingInsights/ServiceCollectionExtensions.cs
@@ -8,18 +8,35 @@ using HotelPricingInsights.Controllers.HotelPriceComparison;
 using HotelPricingInsights.Controllers.HotelPriceComparison.CurrencyConversion;
 using HotelPricingInsights.Controllers.HotelPriceComparison.CurrencyExchangeRates;
 using HotelPricingInsights.Controllers.HotelPriceComparison.PricingExtractsForHotelsInSpecificPeriod;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.Memory;
 
 namespace HotelPricingInsights;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddPricingComparisonServices(this IServiceCollection services)
+    public static void AddPricingComparisonServices(this IServiceCollection services)
     {
+        services.AddMemoryCache();
+
+        services.AddSingleton<AsyncCachePolicy<CurrencyExchangeRate?>>(sp =>
+        {
+            var cacheProvider = new MemoryCacheProvider(sp.GetRequiredService<IMemoryCache>());
+            return Policy.CacheAsync<CurrencyExchangeRate?>(cacheProvider, TimeSpan.FromMinutes(10));
+        });
+
         services.AddScoped<IHotelPricingComparisonService, HotelPricingComparisonService>();
         services.AddScoped<ICurrencyConverter, CurrencyConverter>();
-        services.AddScoped<ICurrencyExchangeRatesDataService, MonthAnchoredCurrencyExchangeRatesDataService>();
+        services.AddScoped<MonthAnchoredCurrencyExchangeRatesDataService>();
+        services.AddScoped<ICurrencyExchangeRatesDataService>(serviceProvider =>
+        {
+            var inner = serviceProvider.GetRequiredService<MonthAnchoredCurrencyExchangeRatesDataService>();
+            var cachePolicy = serviceProvider.GetRequiredService<AsyncCachePolicy<CurrencyExchangeRate?>>();
+            return new CachingCurrencyExchangeRatesDataService(inner, cachePolicy);
+        });
         services.AddScoped<IPricingExtractsForHotelsInSpecificPeriodDataService, PricingExtractsForHotelsInSpecificPeriodDataService>();
         services.AddTransient<IValidator<PricingComparisonRequest>, PricingComparisonRequestValidator>();
         services.AddTransient<PricingComparisonController>();
@@ -27,7 +44,5 @@ public static class ServiceCollectionExtensions
         services.TryAddSingleton<BigtableClient>(_ => throw new InvalidOperationException("Configure a BigtableClient instance via dependency injection."));
         services.TryAddSingleton<Table>(_ => new Table { Name = TableName.FromProjectInstanceTable("project", "instance", "pricing").ToString() });
         services.TryAddSingleton<Func<IDbConnection>>(_ => throw new InvalidOperationException("Configure a database connection factory via dependency injection."));
-
-        return services;
     }
 }
